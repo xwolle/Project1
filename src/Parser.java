e1d99620846fbf64be0f9e6f6978d82014f0d13d
xander <xander@192.168.1.47>
2020-01-28 11:39:59 -0500

    commenting headers

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,15 @@
-
+/**
+ * Class that handles all of the possible cases
+ * from the input file
+ * 
+ * @author xander <xander1>
+ * @version <1.28.20>
+ *
+ */
 public class Parser {
+
+    
+    
+    
 
 }
--- src/ProjectRunner.java
+++ src/ProjectRunner.java
@@ -1,4 +1,10 @@
-
+/**
+ * main class runner
+ * 
+ * @author xander <xander1>
+ * @version <1.28.20>
+ *
+ */
 public class ProjectRunner {
 
 }
