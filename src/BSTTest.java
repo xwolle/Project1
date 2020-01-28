ec504ded399341bd6dbd5e26c734f5427c603929
xander <xander@192.168.1.47>
2020-01-27 19:18:05 -0500

    bst tests

--- src/BSTTest.java
+++ src/BSTTest.java
@@ -11,7 +11,7 @@ public class BSTTest extends TestCase {
         
     }
     
-    //simple insert test
+    //simple insert test with up to 3
     public void testInsert() {
         
         test.insert("xander");
@@ -22,7 +22,45 @@ public class BSTTest extends TestCase {
         test.insert("dick");
         test.insert("harry");
         
+        assertEquals("tom", test.findMax());
+        assertEquals("dick", test.findMin());
         
+        test.remove("tom");
+        assertEquals("harry", test.findMax());
+        
+    }
+    
+    public void testRemove() {
+        
+        test.insert("xander");
+        assertFalse(test.isEmpty());
+        
+        test.remove("xander");
+        assertTrue(test.isEmpty());
+        
+        test.insert("a");
+        test.insert("b");
+        test.insert("c");
+        test.insert("d");
+        test.insert("e");
+        test.insert("f");
+        test.insert("g");
+        test.insert("h");
+        test.insert("i");
+        test.insert("j");
+        
+        test.remove("a");
+        test.remove("b");
+        test.remove("c");
+        test.remove("d");
+        test.remove("e");
+        test.remove("f");
+        test.remove("g");
+        test.remove("h");
+        test.remove("i");
+        test.remove("j");
+        
+        assertTrue(test.isEmpty());
         
     }
     
