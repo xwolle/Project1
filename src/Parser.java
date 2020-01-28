3ff516c006aaa9f4abac553a82789a8bb177bf92
xander <xander@192.168.1.47>
2020-01-28 12:41:46 -0500

    parser

--- src/Parser.java
+++ src/Parser.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Class that handles all of the possible cases
  * from the input file
@@ -6,10 +10,58 @@
  * @version <1.28.20>
  *
  */
-public class Parser {
+public class Parser extends BST {
 
+    /**
+     * fields
+     */
+    private BST shapes;
+    private Scanner fileInput;
+    private Shape lastShape;
     
     
+    /**
+     * constructor
+     */
+    public Parser(String file) throws FileNotFoundException {
+        
+        shapes = new BST();
+        fileInput = new Scanner(new File(file));
+        lastShape = null;
+        
+        while (fileInput.hasNextLine()) {
+            
+            parseData();
+            
+        }
+        
+    }
+    
+    /**
+     * parse data
+     */
+    public void parseData() {
+        
+        String next = fileInput.nextLine();
+        
+        if (!next.equals("")) {
+            
+            next = next.trim();
+            String[] nextArr = next.split("\\s+");
+            
+            
+            if (nextArr[0].equalsIgnoreCase("insert")) {
+                
+                if (nextArr[4] <= 0 || nextArr[5] <= 0) {
+                    
+                    
+                    
+                }
+                
+                
+            }
+        }
+    }
     
 
 }
