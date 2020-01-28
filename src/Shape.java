041464f5175503bce1106fe339a2aa45e9c4b0c8
xander <xander@192.168.1.47>
2020-01-28 11:16:51 -0500

    added shape class

---
+++ src/Shape.java
@@ -0,0 +1,15 @@
+/**
+ * Will contain all attributes of any object added
+ * @author xander <xander1>
+ * @version <1.28.20>
+ *
+ */
+public class Shape {
+
+    /**
+     * instance fields
+     */
+    
+    
+    
+}
